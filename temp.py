0f93fd7af8f2b91de8cb1ba756d8dd63f99507b4
Josh Loecker <JoshLoecker@iCloud.com>
2020-07-28 17:18:40 -0500

    Modified guppy_aligner rule, double checking that things are working as expected
    Signed-off-by: Josh Loecker <JoshLoecker@iCloud.com>

--- temp.py
+++
@@ -1,3 +0,0 @@
-barcode_numbers=glob_wildcards(config['results_folder'] + "Barcode/{barcode_number}.merged.fastq").barcode_number),
-barcode_number=glob_wildcards(config['results_folder'] + "Barcode/{barcode_number}.merged.fastq").barcode_number),
-barcode_number=glob_wildcards(config['results_folder'] + "Barcode/{barcode_number}.merged.fastq").barcode_number),
